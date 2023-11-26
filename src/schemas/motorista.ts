06eecfd60f94122cf620a0e82337f3f2c5378d25
aquiles23 <jjoseaquiless@gmail.com>
2023-11-26 16:51:43 -0300

    valida categoria cnh nas opções disponiveis

--- src/schemas/motorista.ts
+++ src/schemas/motorista.ts
@@ -4,5 +4,10 @@ export const motoristaSchema = z.object({
   cpf: z.string().max(12),
   nome: z.string().max(500),
   vencimento_cnh: z.union([z.string(), z.date()]),
-  categoria_cnh: z.string().max(2),
+  categoria_cnh: z.union([
+    z.literal("A"),
+    z.literal("B"),
+    z.literal("AB"),
+    //, z.literal("D"), z.literal("E")
+  ]),
 });
